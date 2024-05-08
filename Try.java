771060612d37679dcf35e188876d4aa0b64b939c
Kishanp48 <kishanpanchal.ghub@gmail.com>
2024-05-08 17:37:30 +0530

    try

---
+++ Try.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+class Try
+{
+
+    JFrame frame;
+
+    Try()
+    {
+
+        frame = new JFrame();
+
+        JButton b1 = new JButton("1");
+        JButton b2 = new JButton("2");
+        JButton b3 = new JButton("3");
+        JButton b4 = new JButton("4");
+        JButton b5 = new JButton("5");
+        JButton b6 = new JButton("6");
+        JButton b7 = new JButton("7");
+        JButton b8 = new JButton("8");
+        JButton b9 = new JButton("9");
+        JButton b10 = new JButton("10");
+
+
+
+        frame.add(b1);
+        frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
+        frame.add(b5);
+        frame.add(b6);
+        frame.add(b7);
+        frame.add(b8);
+        frame.add(b9);
+        frame.add(b10);
+
+
+        frame.setLayout(new FlowLayout());
+
+        frame.setSize(300,300);
+
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String args[])
+    {
+
+        new Try();
+
+    }
+
+}
