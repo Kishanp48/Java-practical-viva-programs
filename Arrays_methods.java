b690940f745ad7195bd4e60bab63fdaefaf94508
Kishan Panchal <124263372+Kishanp48@users.noreply.github.com>
2024-05-08 21:35:36 +0530

    *Updated

--- Arrays_methods.java
+++ Arrays_methods.java
@@ -20,13 +20,18 @@ public class Arrays_methods {
         int arr[] = {1,2,3,4,5};
 
         Arrays.fill(arr, 1, 3, 8);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));   // [1, 8, 8, 4, 5]
+
+
+    
+
+
 
 
         Arrays.fill(arr, 5);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));   // [5, 5, 5, 5, 5]
 
-
+        
 
 
 
@@ -34,8 +39,10 @@ public class Arrays_methods {
 
         int arr0[] = {1,2,3,4,5};
         
-        System.out.println(Arrays.binarySearch(arr0, 2));
+        System.out.println(Arrays.binarySearch(arr0, 2));   // 1
 
+
+        
 
 
         
@@ -44,8 +51,8 @@ public class Arrays_methods {
         int arr3[] = {1,2,3};
         
 
-        System.out.println(Arrays.equals(arr1, arr2));
-        System.out.println(Arrays.equals(arr1, arr3));
+        System.out.println(Arrays.equals(arr1, arr2));  // true
+        System.out.println(Arrays.equals(arr1, arr3));  // false
 
 
 
@@ -58,7 +65,10 @@ public class Arrays_methods {
 
         Arrays.sort(b, Collections.reverseOrder());
         
-        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(b)); // [56, 24, 23, 23, 7, 2]
+
+
+        
 
 
     }
